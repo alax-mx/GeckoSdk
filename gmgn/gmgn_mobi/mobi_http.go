da3cd8fe5f22d4becc9034b76ed2adfc567c0e57
alax-mx <lyf378015683@aliyun.com>
2025-08-31 01:13:08 +0800

    跟新策略

--- gmgn/gmgn_mobi/mobi_http.go
+++ gmgn/gmgn_mobi/mobi_http.go
@@ -4,36 +4,76 @@ import (
 	"bytes"
 	"crypto/tls"
 	"io"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/alax-mx/geckosdk/proxy"
 )
+
+var g_configList []*tls.Config
+
+func GetRanomClpherSuites() *tls.Config {
+	if len(g_configList) <= 0 {
+		g_configList = append(g_configList, &tls.Config{
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			},
+			MinVersion: tls.VersionTLS10,
+			MaxVersion: tls.VersionTLS13,
+			//可以添加其他配置，如支持的Extensions等
+		})
+		g_configList = append(g_configList, &tls.Config{
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			},
+			MinVersion: tls.VersionTLS10,
+			MaxVersion: tls.VersionTLS13,
+			//可以添加其他配置，如支持的Extensions等
+		})
+		g_configList = append(g_configList, &tls.Config{
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			},
+			MinVersion: tls.VersionTLS10,
+			MaxVersion: tls.VersionTLS13,
+			//可以添加其他配置，如支持的Extensions等
+		})
+		g_configList = append(g_configList, &tls.Config{
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			},
+			MinVersion: tls.VersionTLS10,
+			MaxVersion: tls.VersionTLS13,
+			//可以添加其他配置，如支持的Extensions等
+		})
+		g_configList = append(g_configList, &tls.Config{
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			},
+			MinVersion: tls.VersionTLS10,
+			MaxVersion: tls.VersionTLS13,
+			//可以添加其他配置，如支持的Extensions等
+		})
+	}
+
+	tmpRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return g_configList[tmpRand.Intn(5)]
+}
 
 func HttpGet(url string, proxyInfo *proxy.STProxyInfo) ([]byte, error) {
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("user-agent", "okhttp/4.9.2")
 	req.Header.Add("Content-Type", "application/json; charset=utf-8")
-	tlsConfig := &tls.Config{
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
-		// 可以添加其他配置，如支持的Extensions等
-	}
 	// client := http.DefaultClient
 	client := &http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
+			TLSClientConfig: GetRanomClpherSuites(),
 		},
 	}
 	if proxyInfo != nil {
